1d42a61a45cffa9401d0ef37a7f7205d14200c7d
Daniel Libanori <daniel.lb68@gmail.com>
2023-07-12 02:06:31 -0300

    Created new chapter modal

--- src/views/chapters/newChapterModal.js
+++ src/views/chapters/newChapterModal.js
@@ -1,4 +1,4 @@
-import { Text, Button, Input } from '@chakra-ui/react'
+import { Text, Button, Input, Select } from '@chakra-ui/react'
 import {
     Modal,
     ModalOverlay,
@@ -15,10 +15,11 @@ import { GlobalContext } from '../../context/globalState';
 export function NewChapterModal({ isOpen, onOpen, onClose }) {
 
     const {addNewBook} = useContext(GlobalContext)
-    const [bookName, setBookName] = useState('')
+    const [creationStep, setCreationStep] = useState(1)
+    const [chapterName, setChapterName] = useState('')
 
     const onCreatePress = () => {
-        addNewBook(bookName)
+        
 
 
 
@@ -31,22 +32,51 @@ export function NewChapterModal({ isOpen, onOpen, onClose }) {
                 <ModalHeader>Modal Title</ModalHeader>
                 <ModalCloseButton />
                 <ModalBody>
-                    <Text>
-                        Parabéns, você deu o primeiro passo para criar seu livro!
-                    </Text>
-                    <Text mt={5}>
-                        Qual será o nome do seu livro?
-                    </Text>
-                    <Input placeholder='Insira aqui o nome do seu novo livro...' value={bookName}
-                        onChange={(e)=> setBookName(e.target.value)}/>
+                    {creationStep === 1 &&
+                        <>
+                            <Text>
+                                Agora vamos criar um capítulo para seu livro!
+                            </Text>
+                            <Text mt={5}>
+                                Qual será o nome desse capítulo?
+                            </Text>
+                            <Input placeholder='Insira aqui o nome do seu novo capítulo...' value={chapterName}
+                                onChange={(e)=> setChapterName(e.target.value)}/>
+                        </>
+                    }
+
+                    {creationStep === 2 &&
+                        <>
+                            <Text mt={5}>
+                                Qual será o mapa desse capítulo?
+                            </Text>
+                            <Select placeholder='Escolha o Mapa'>
+                                <option value='1'>Museu</option>
+                            </Select>
+                        </>
+                    }
                 </ModalBody>
 
-                <ModalFooter mt={10}>
+                <ModalFooter mt={10} display='flex' justifyContent='flex-end'>
 
 
-                    <Button colorScheme='blue' mr={3} onClick={onCreatePress}>
-                        CRIAR !
-                    </Button>
+                    {creationStep === 1 &&                   
+                        <Button colorScheme='blue' mr={3} onClick={()=>setCreationStep(2)}>
+                            Próximo Passo
+                        </Button>
+                    }
+                    
+
+                    {creationStep === 2 &&                   
+                        <>
+                            <Button colorScheme='blue' mr={3} onClick={()=>setCreationStep(1)}>
+                                Voltar
+                            </Button>
+                            <Button colorScheme='blue' mr={3} onClick={onCreatePress}>
+                                CRIAR!!!
+                            </Button>
+                        </>
+                    }
                 </ModalFooter>
             </ModalContent>
         </Modal>
