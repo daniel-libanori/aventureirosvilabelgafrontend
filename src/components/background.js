8a341406fc22fc562f204b7f86ad29ceefabeeba
Daniel Libanori <daniel.lb68@gmail.com>
2024-05-06 04:07:44 -0300

    fix background

--- src/components/background.js
+++ src/components/background.js
@@ -1,27 +1,31 @@
-import { Flex } from '@chakra-ui/react';
-import React from 'react';
-import styled from 'styled-components';
-import PaperTexture from '../assets/paperbackgroundTexture.jpg'
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import styled from "styled-components";
+import PaperTexture from "../assets/paperbackgroundTexture.jpg";
 
-function Background({children, editchapter = false}) {
-    return (
-        <StyledFlex editchapter={editchapter}>
-            {children}
-        </StyledFlex>
-    );
+function Background({ children, editchapter = false, mediumSize = false }) {
+  return (
+    <StyledFlex editchapter={editchapter} mediumSize={mediumSize}>
+      {children}
+    </StyledFlex>
+  );
 }
 
 export default Background;
 
-
 const StyledFlex = styled(Flex)`
-    width: 100vw;
-    height: ${props => !!props.editchapter ? window.innerHeight*3 + "px" : "100vh"};
-    background-image: url(${PaperTexture});
-    background-color: #bf6b3f;
-    background-blend-mode: multiply;
-    background-size: auto;
-    background-repeat: repeat;
-    align-items: center;
-    justify-content: center;
+  width: 100vw;
+  height: ${(props) =>
+    !!props.editchapter
+      ? window.innerHeight * 2 + "px"
+      : props.mediumSize
+      ? "140vh"
+      : "100vh"};
+  background-image: url(${PaperTexture});
+  background-color: #bf6b3f;
+  background-blend-mode: multiply;
+  background-size: auto;
+  background-repeat: repeat;
+  align-items: center;
+  justify-content: center;
 `;
